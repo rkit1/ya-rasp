4b7f87366762d276fb4559a15cfeea66a604b679
Victor Gorokhov <me@rkit.pp.ru>
2024-02-16 13:51:22 +0300

    станции от-до

---
+++ src/app/api.service.ts
@@ -0,0 +1,132 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import * as O from 'rxjs';
+import * as Op from 'rxjs/operators';
+
+const api_key = 'd881c559-1a00-4405-be1e-41371b60f0fe';
+
+export interface Pagination {
+  'pagination': {
+    'total': number,
+    'limit': number,
+    'offset': number,
+  },
+}
+
+export type Transport_type = 'plane' | 'train' | 'suburban' | 'bus' | 'sea' | 'river' | 'helicopter';
+const Transport_type_strings: Map<Transport_type, string> = new Map([
+  ['plane', 'самолет'],
+  ['train', 'поезд'],
+  ['suburban', 'электричка'],
+  ['bus', 'автобус'],
+  ['sea', 'морской транспорт'],
+  ['river', 'речной транспорт'],
+  ['helicopter', 'вертолет'],
+]);
+
+export interface Station_nearest {
+  'distance': number,
+  'code': string,
+  'station_type': string,
+  'station_type_name': string,
+  'type_choices': {
+    'schedule': {
+      'desktop_url': string//'https://rasp.yandex.ru/station/9761931/schedule',
+      'touch_url': string
+    }
+  },
+  'title': string,
+  // 'popular_title': '',
+  // 'short_title': '',
+  'transport_type': Transport_type,
+  // 'lat': 50.4516962252837,
+  // 'lng': 40.1392928134917,
+  // 'type': 'station'
+}
+
+export interface Stations_nearest extends Pagination {
+  'stations': Station_nearest[],
+}
+
+export interface Station_schedule extends Pagination {
+  "schedule": [{
+    "thread": {
+      "uid": string,
+    }
+  },],
+  "interval_schedule": [{
+    "thread": {
+      "uid": string,
+    },
+  },],
+}
+
+export interface Thread_schedule {
+  "title": string,
+  "stops": [{
+    "station": {
+      "title": string,
+      "code": string,
+    },
+  },]
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  http = inject(HttpClient);
+
+  tansport_type_strings: Map<Transport_type, string> = new Map([
+    ['plane', 'самолет'],
+    ['train', 'поезд'],
+    ['suburban', 'электричка'],
+    ['bus', 'автобус'],
+    ['sea', 'морской транспорт'],
+    ['river', 'речной транспорт'],
+    ['helicopter', 'вертолет'],
+  ]);
+
+  get_stations(type: Transport_type) {
+    return this.http.get<Stations_nearest>(
+      `/api/v3.0/nearest_stations/?apikey=${api_key}` +
+      `&lat=55.80136114609288&lng=37.560503846329894&distance=50&lang=ru_RU&transport_types=${type}`
+    )
+  }
+
+  // Возвращает uid всех линий
+  get_threads(uid: string): O.Observable<Set<string>> {
+    return this.http.get<Station_schedule>(
+      `/api/v3.0/schedule/?apikey=${api_key}` +
+      `&station=${uid}`
+    ).pipe(Op.map(sch => {
+      const thr = new Set<string>();
+      for (const ent of sch.schedule) {
+        thr.add(ent.thread.uid);
+      }
+      for (const ent of sch.interval_schedule) {
+        thr.add(ent.thread.uid);
+      }
+      return thr;
+    }));
+  }
+
+  get_destination_stations(routes: Set<string>, from_uid: string): O.Observable<Map<string, string>> {
+    const arr: O.Observable<Thread_schedule>[] = [];
+    routes.forEach(route_uid => arr.push(this.http.get<Thread_schedule>(
+      `/api/v3.0/thread/?apikey=${api_key}` +
+      `&uid=${route_uid}&from=${from_uid}` // date
+    )));
+    return O.forkJoin(arr).pipe(Op.map(x => {
+      const m = new Map<string, string>();
+      x.forEach(val => {
+        for (const st of val.stops) {
+          m.set(st.station.code, st.station.title);
+        }
+      });
+      return m;
+    }));
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService, Station_nearest, Stations_nearest, Transport_type } from './api.service';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import * as O from 'rxjs';
+import * as Op from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  providers: [ApiService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'rasp';
+  api = inject(ApiService);
+
+  transport_type?: Transport_type;
+  stations_from?: Stations_nearest;
+  station_from?: string;
+  stations_to?: Map<string, string>;
+  station_to = "";
+
+
+  change_transport_type() {
+    delete this.stations_from;
+    delete this.stations_to;
+    delete this.station_from;
+    this.station_to = "";
+    this.api.get_stations(this.transport_type!).subscribe(stations => {
+      this.stations_from = stations;
+    });
+  }
+
+  change_station_from() {
+    delete this.stations_to;
+    this.station_to = "";
+    if (!this.station_from) {
+      return
+    }
+    
+    this.api.get_threads(this.station_from).pipe(Op.switchMap(threads => {
+      if (!this.station_from) {
+        return O.EMPTY;
+      }
+      return this.api.get_destination_stations(threads, this.station_from);
+    })).subscribe(destinations => {
+      this.stations_to = destinations; 
+    });
+  }
+
+  constructor() {
+    console.log(this.constructor.name, this);
+  }
 }
